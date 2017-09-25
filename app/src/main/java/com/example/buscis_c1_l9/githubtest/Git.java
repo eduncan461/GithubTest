4404f6ab7857c01f6f20ab759c751348c52ef488
BUSCIS-C1-L9 <BUSCIS-C1-L9@scc.ad.losrios.edu>
2017-09-25 16:15:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/buscis_c1_l9/githubtest/Git.java
@@ -0,0 +1,13 @@
+package com.example.buscis_c1_l9.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Git extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+}
